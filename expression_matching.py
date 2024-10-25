3079c68661b59b8800d21433051de7e30a282513
constance khalumba <constancekhalumba@gmail.com>
2024-10-25 14:43:58 +0300

    first commit

--- expression_matching.py
+++ expression_matching.py
@@ -1,14 +1,29 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         # Top-down Memorization
+        memo = {}
+        
+        def dfs(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            
+            if i >= len(s) and j >= len(p):
+                return True
+            if j >= len(p):
+                return False
+            
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+            
+            if (j + 1) < len(p) and p[j + 1] == "*":
+                # Don't use '*' (skip `p[j]` and `p[j+1]`) or use '*' (move `i` forward if there's a match)
+                memo[(i, j)] = dfs(i, j + 2) or (match and dfs(i + 1, j))
+                return memo[(i, j)]
+            
+            if match:
+                memo[(i, j)] = dfs(i + 1, j + 1)
+                return memo[(i, j)]
+            
+            memo[(i, j)] = False
+            return False
 
-            def dfs(i, j):
-                  if i >= len(s) and j >= len(p):
-                        return True
-                  if j >= len(s):
-                        return False
-                  
-                  match = i < len(s) and (s[i] == p[j] or p[j] = ".")
-                  if (j + 1) < len(p) and p[j + 1] == "*":
-                        dfs(i, j + 2) or #don't use *
-                        
+        return dfs(0, 0)
